635666bdd7053c804aaea4183d8f1016c83e410a
niko3124 <nikoru24o7@gmail.com>
2024-03-27 15:33:36 +0300

    8

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         System.out.printf("Hel an welcoome!!");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 3; i <= 5; i++) {
+        for (int i = 3; i <= 7; i++) {
 
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Ctrl+F8.
